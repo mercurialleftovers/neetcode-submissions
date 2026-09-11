89f15b7ec7d8a509195cde4e740490bf4dfd1f92
Mercurial Leftovers <mercurialleftovers@proton.me>
2026-09-11 12:09:55 +0100

    Add: anagram-groups - submission-6

---
+++ Data Structures & Algorithms/anagram-groups/submission-6.py
@@ -0,0 +1,37 @@
+class Solution:
+    def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
+        hashes:list[tuple[str,...]] = [tuple(sorted(word)) for word in strs]
+        groups:dict[tuple[str,...], list[str]] = {} # TODO(bader): turn into an ordered dict (fifo)
+                
+        for i in range(len(hashes)):
+            if hashes[i] in groups:
+                groups[hashes[i]].append(strs[i])
+            else:
+                groups[hashes[i]] = [strs[i],]
+
+        return list(groups.values())
+
+
+
+    def anagrams(self, s:str, t:str) -> bool:
+        word1_letter_frequency: dict[str, int] = {}
+        word2_letter_frequency: dict[str, int] = {}
+
+        for letter in s:
+            if letter in word1_letter_frequency:
+                word1_letter_frequency[letter] += 1
+            else:
+                word1_letter_frequency[letter] = 1
+
+            
+        for letter in t:
+            if letter in word2_letter_frequency:
+                word2_letter_frequency[letter] += 1
+            else:
+                word2_letter_frequency[letter] = 1
+        
+        for letter in word1_letter_frequency:
+            if not letter in word2_letter_frequency or not word1_letter_frequency[letter] == word2_letter_frequency[letter]:
+                return False 
+        
+        return len(word1_letter_frequency.keys()) == len(word2_letter_frequency.keys())
